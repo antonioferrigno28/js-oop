adbdb42eb2cf7dfc41fd8d170d26d24059054b83
antonioferrigno28 <ferrigno1@hotmail.it>
2024-11-13 15:04:01 +0100

    Bonus done!

--- JS/script.js
+++ JS/script.js
@@ -18,6 +18,18 @@ class Veicolo {
   }
 }
 
+class Automobile extends Veicolo {
+  constructor(brand, year, color, doors, gasType) {
+    super(brand, year, color);
+    this.doors = doors;
+    this.gasType = gasType;
+  }
+
+  informazioniAutomobile() {
+    return this.doors + " " + this.gasType;
+  }
+}
+
 const nowDate = new Date();
 const nowYear = nowDate.getFullYear();
 
@@ -26,3 +38,9 @@ const tonyCar = new Veicolo("Fiat", 2019, "Blu");
 console.log(`Ecco le informazioni del veicolo: ${tonyCar.informazioni()}`);
 
 console.log(`L'età del veicolo è: ${tonyCar.calcoloEta()} anni`);
+
+const tizianoCar = new Automobile("Opel", 2020, "Nera", 4, "benzina");
+
+console.log(
+  `I dettagli "porte" e "Tipo di gas" sono: ${tizianoCar.informazioniAutomobile()}`
+);
