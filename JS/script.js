f47723cc70c621139a3bf1dd430fd1eae9001afa
antonioferrigno28 <ferrigno1@hotmail.it>
2024-11-13 10:27:06 +0100

    done! no bonus

--- JS/script.js
+++ JS/script.js
@@ -12,8 +12,17 @@ class Veicolo {
   informazioni() {
     return this.brand + " " + this.year + " " + this.color;
   }
+
+  calcoloEta() {
+    return nowYear - this.year;
+  }
 }
+
+const nowDate = new Date();
+const nowYear = nowDate.getFullYear();
 
 const tonyCar = new Veicolo("Fiat", 2019, "Blu");
 
-console.log(tonyCar.informazioni());
+console.log(`Ecco le informazioni del veicolo: ${tonyCar.informazioni()}`);
+
+console.log(`L'età del veicolo è: ${tonyCar.calcoloEta()} anni`);
